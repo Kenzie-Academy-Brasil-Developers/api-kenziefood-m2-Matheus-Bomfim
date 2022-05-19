36b43bc260391bbbc35f27ccc4d5afb69fe4beab
nunesleo04@gmail.com <nunesleo04@gmail.com>
2022-05-19 17:00:25 -0300

    Criação de edição e cadastro produtos

--- src/js/scriptDashBoard.js
+++ src/js/scriptDashBoard.js
@@ -165,6 +165,150 @@ function loginUsuario(){
       </li>`;
    
 };
+templateDashboard();
+
+
+const addNovoProduto = document.querySelector('#addNovoProduto')
+const cadastrarProdutos = document.querySelector('.cadastrarProdutoContainer')
+addNovoProduto.addEventListener("click", function(){
+    focus.style.display = 'flex'
+    cadastrarProdutos.style.display = 'flex'
+    const fecharCadastro = document.querySelector('#fecharCadastro')
+})
+
+fecharCadastro.addEventListener("click", function(){
+    focus.style.display = 'none'
+    cadastrarProdutos.style.display = 'none'
+
+    
+})
+// const editarProduto = document.getElementById('acoesEditar')
+const editarProdutoContainer = document.querySelector('.editarProdutoContainer')
+const excluirProdutoContainer = document.querySelector('#confirmarExclusao')
+const focus = document.querySelector('#focusBackground')
+
+document.addEventListener("click",function(e){
+    if(e.target.id == 'acoesEditar'){
+        focus.style.display = 'flex'
+        editarProdutoContainer.style.display = 'flex'
+        const fecharEditar = document.querySelector('#fecharEditar')
+    }
+    if(e.target.id == 'acoesExcluir'){
+        focus.style.display = 'flex'
+
+        excluirProdutoContainer.style.display = 'flex'
+        const fecharExclusao = document.querySelector("#fecharExclusao")
+    }
+})
+
+fecharEditar.addEventListener('click', function(){
+    focus.style.display = 'none'
+    editarProdutoContainer.style.display = 'none'
+})
+fecharExclusao.addEventListener('click', function(){
+    focus.style.display = 'none'
+    excluirProdutoContainer.style.display = 'none'
+})
+function addProduto(){
+    const objCadastrar = {}
+    const inputNome = document.querySelector('.cadastroNome')
+    const inputDescricao = document.querySelector('.cadastroDescricao')
+    const inputValor = document.querySelector('.cadastroValor')
+    const inputImg = document.querySelector('.cadastroImg')
+    const botaoCadastrar = document.querySelector('#botaoCadastrar')
+
+    const categoriaPani = document.querySelector('#cadastroCategoriaPani')
+    const categoriaBebidas = document.querySelector('#cadastroCategoriaBebidas')
+    const categoriaFrutas = document.querySelector('#cadastroCategoriaFrutas')
+    categoriaPani.addEventListener('click', function(){
+        objCadastrar.categoria = ''
+        objCadastrar.categoria = 'Panificadora'
+        categoriaPani.classList.remove('categoriaLi')
+        categoriaPani.classList.add('categoriaAtivado')
+        categoriaBebidas.classList.remove('categoriaAtivado')
+        categoriaFrutas.classList.remove('categoriaAtivado')
+    })
+    categoriaBebidas.addEventListener('click', function(){
+        objCadastrar.categoria = ''
+        objCadastrar.categoria = 'Bebidas'
+        categoriaBebidas.classList.remove('categoriaLi')
+        categoriaBebidas.classList.add('categoriaAtivado')
+        categoriaPani.classList.remove('categoriaAtivado')
+        categoriaFrutas.classList.remove('categoriaAtivado')
+    })
+    categoriaFrutas.addEventListener('click', function(){
+        objCadastrar.categoria = ''
+        objCadastrar.categoria = 'Frutas'
+        categoriaFrutas.classList.remove('categoriaLi')
+        categoriaFrutas.classList.add('categoriaAtivado')
+        categoriaBebidas.classList.remove('categoriaAtivado')
+        categoriaPani.classList.remove('categoriaAtivado')
+    })
+
+    
+    
+
+    botaoCadastrar.addEventListener('click', function(){
+        objCadastrar.nome = inputNome.value
+        objCadastrar.descricao = inputDescricao.value
+        objCadastrar.preco = inputValor.value
+        objCadastrar.imagem = inputImg.value
+        console.log(objCadastrar)
+        focus.style.display = 'none'
+        cadastrarProdutos.style.display = 'none'
+    })
+}
+addProduto()
+
+function editarProduto(){
+    const objEditar = {}
+    const inputNome = document.querySelector('.editarNome')
+    const inputDescricao = document.querySelector('.editarDescricao')
+    const inputValor = document.querySelector('.editarValor')
+    const inputImg = document.querySelector('.editarImg')
+    const salvarAlteracoes = document.querySelector('#botaoEditarSalvar')
+
+    const categoriaPani = document.querySelector('#editarCategoriaPani')
+    const categoriaBebidas = document.querySelector('#editarCategoriaBebidas')
+    const categoriaFrutas = document.querySelector('#editarCategoriaFrutas')
+    categoriaPani.addEventListener('click', function(){
+        objEditar.categoria = ''
+        objEditar.categoria = 'Panificadora'
+        categoriaPani.classList.remove('categoriaLi')
+        categoriaPani.classList.add('categoriaAtivado')
+        categoriaBebidas.classList.remove('categoriaAtivado')
+        categoriaFrutas.classList.remove('categoriaAtivado')
+    })
+    categoriaBebidas.addEventListener('click', function(){
+        objEditar.categoria = ''
+        objEditar.categoria = 'Bebidas'
+        categoriaBebidas.classList.remove('categoriaLi')
+        categoriaBebidas.classList.add('categoriaAtivado')
+        categoriaPani.classList.remove('categoriaAtivado')
+        categoriaFrutas.classList.remove('categoriaAtivado')
+    })
+    categoriaFrutas.addEventListener('click', function(){
+        objEditar.categoria = ''
+        objEditar.categoria = 'Frutas'
+        categoriaFrutas.classList.remove('categoriaLi')
+        categoriaFrutas.classList.add('categoriaAtivado')
+        categoriaBebidas.classList.remove('categoriaAtivado')
+        categoriaPani.classList.remove('categoriaAtivado')
+    })
+
+
+    salvarAlteracoes.addEventListener('click', function(){
+        console.log('oi')
+        objEditar.nome = inputNome.value
+        objEditar.descricao = inputDescricao.value
+        objEditar.preco = inputValor.value
+        objEditar.imagem = inputImg.value
+        console.log(objEditar)
+        focus.style.display = 'none'
+        editarProdutoContainer.style.display = 'none'
+    })
+}
+editarProduto()
 
 const listarProdutosDashboard = document.querySelector('#listaDeProdutosDashboard');
 
